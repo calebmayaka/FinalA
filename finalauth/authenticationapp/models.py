aa55743fbc06fe9a0f2cd0456d1da0371210becc
Caleb Mayaka <mayakaombogo254@gmail.com>
2025-03-17 17:03:37 +0300

    Update models.py

--- finalauth/authenticationapp/models.py
+++ finalauth/authenticationapp/models.py
@@ -1,26 +1,68 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.core.validators import MaxValueValidator, MinValueValidator
-from ckeditor.fields import RichtextField
+from ckeditor.fields import RichTextField
+from django.core.validators import MaxvalueValidator, MinValueValidator
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
 
-# for managing authentication of users and superusers
-class MyCustomUserManager(BaseUserManager):
-    # defines the required fields for creating a user
-    # self refers to an instance of the class
-    def create_user(self,email,name,password=None):
-        # email and name validation checks
-        if not email:
-            raise ValueError('Must have an email address')
-        if not name:
-            raise ValueError('Must Have a name')
-        # this creates a user instance using the model using email and name
+class CustomUserManager(BaseUserManager):
+    def create_user(self,name,email,password=None):
+        if email is None:
+            raise ValueError('User Must have an email address')
+        if name is None:
+            raise ValueError('User Must have a name')
         user = self.model(
-            # converting the email to lowercase for consistency
             email=self.normalize_email(email),
-            # setitng the name attribute directly to the name parameter
             name=name,
         )
-        # setting the password for the user, also hashing the password
         user.set_password(password)
         user.save(using=self._db)
-        
+        return user
+    
+    def create_super_user(self,name,email,password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            name = name,
+            password = password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+    
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_applicant = models.BooleanField(default=False)
+    is_company = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False) =
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    # calling the custom user manager
+    objects = CustomUserManager()
+
+    # str dunder method to return the email when the user object is called
+    def __str__(self):
+        return self.email
+    
+    # The has_perm method is used to check if a user has a specific permission.
+    # perm if the type of permission being checked and obj is the object the permission is being checked on
+    def has_perm(self,perm,obj=None):
+        return True
+    
+    def has_module_perms(self,app_label):
+        return True
+    
+    
+    
+
+
+
+
+
+    
+
