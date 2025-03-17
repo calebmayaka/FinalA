6f4a8149011d5d07c006c8ed59e32b486bc1c660
Caleb Mayaka <mayakaombogo254@gmail.com>
2025-03-17 13:38:24 +0300

    Update models.py

--- finalauth/authenticationapp/models.py
+++ finalauth/authenticationapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.validators import MaxValueValidator, MinValueValidator
+from ckeditor.fields import RichtextField
 
-# Create your models here.
+# for managing authentication of users and superusers
+class MyCustomUserManager(BaseUserManager):
+    # defines the required fields for creating a user
+    # self refers to an instance of the class
+    def create_user(self,email,name,password=None):
+        # email and name validation checks
+        if not email:
+            raise ValueError('Must have an email address')
+        if not name:
+            raise ValueError('Must Have a name')
+        # this creates a user instance using the model using email and name
+        user = self.model(
+            # converting the email to lowercase for consistency
+            email=self.normalize_email(email),
+            # setitng the name attribute directly to the name parameter
+            name=name,
+        )
+        # setting the password for the user, also hashing the password
+        user.set_password(password)
+        user.save(using=self._db)
+        
