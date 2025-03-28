2b3993a8aaa629585624a71d3d2baf991425a28c
Caleb Mayaka <mayakaombogo254@gmail.com>
2025-03-28 17:04:07 +0300

    update

--- finalauth/authenticationapp/admin.py
+++ finalauth/authenticationapp/admin.py
@@ -1,3 +1,18 @@
+# importing admin
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'name', 'last_login', 'is_admin', 'is_applicant', 'is_company')
+    search_fields = ('email','name')
+    readonly_fields = ('date_joined', 'last_login')
+    filter_horizontal = ()
+    ordering = ('email',)   
+    fieldsets = ()
+    list_filter = ('is_admin', 'is_active', 'is_applicant', 'is_company')
 
 # Register your models here.
+
+admin.site.register(CustomModel, AccountAdmin)
+admin.site.
